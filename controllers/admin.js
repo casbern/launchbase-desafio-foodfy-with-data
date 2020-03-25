6344b045164f335ebd8d659affbe24c426791dbe
Cássia <cascrisbern@gmail.com>
2020-03-25 14:00:17 -0300

    style and create button save and delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -70,15 +70,52 @@ exports.show = function(req, res) {
     ...foundRecipe
   }
 
-  return res.render("admin/show")
+  return res.render("admin/show", {recipe})
 }
 
 exports.edit = function(req, res) {
-  return res.send("I will edit a recipe")
+  const {id} = req.params
+  const foundRecipe = data.recipes.find(function (recipe) {
+    return recipe.id == id
+  })
+
+  if (!foundRecipe) {
+    return res.send("Recipe was not found!")
+  }
+  
+  const recipe = {
+    ...foundRecipe
+  }
+
+  return res.render("admin/edit", {recipe})
 }
 
+
+
+
 exports.put = function(req, res) {
-  return res.send("I will edit a recipe")
+  const {id} = req.body
+  let index = 0
+
+  const foundRecipe = data.recipes.find(function (recipe, foundIndex) {
+    if (id == recipe.id) {
+      index = foundIndex
+      return true
+    }
+  })
+
+    const recipe = {
+      ...foundRecipe,
+      ...req.body
+    }
+
+    data.recipes[index] = recipe
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+      if(err) return res.send("Write error!")
+  
+      return res.redirect(`/recipes/${id}`)
+    })
 }
 
 exports.delete = function(req, res) {
--- public/admin/add-ingredients.js
+++ public/admin/add-ingredients.js
@@ -1,14 +1,13 @@
 function addIngredient() {
-  console.log("i was clicked")
   const ingredients = document.querySelector("#ingredients")
   const fieldContainer = document.querySelectorAll(".ingredient")
 
   const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
-
+    
   if(newField.children[0].value == "") {
     return false
   }
-
+  
   newField.children[0].value = ""
   ingredients.appendChild(newField)
 }
